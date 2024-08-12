f66b88d610529e3fadc51326c2f93b8a785ab731
Muhammad Caessar <137273027+CizerSkizerBigger@users.noreply.github.com>
2024-08-12 13:51:13 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,14 +2,6 @@ import streamlit as st # type: ignore
 
 st.title('Select box Example')
 
-st.header('st.selectbox')
-
-option = st.selectbox(
-     'What is your favorite color?',
-     ('Blue', 'Red', 'Green'))
-
-st.write('Your favorite color is ', option)
-
 #=========
 
 st.header('st.selectbox')
