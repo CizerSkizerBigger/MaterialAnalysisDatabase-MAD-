ab499a99259efd21dfcd50f8778a054918868ba1
Muhammad Caessar <137273027+CizerSkizerBigger@users.noreply.github.com>
2024-08-12 13:54:54 +0700

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ st.title('Material Analysis')
 
 
 st.sidebar.header('About')
-st.write("Eg")
+st.sidebar.write("Eg")
 user_name = st.sidebar.text_input('What is your name?')
 user_emoji = st.sidebar.selectbox('Choose an emoji', ['', '😄', '😆', '😊', '😍', '😴', '😕', '😱'])
 user_food = st.sidebar.selectbox('What is your favorite food?', ['', 'Tom Yum Kung', 'Burrito', 'Lasagna', 'Hamburger', 'Pizza'])
